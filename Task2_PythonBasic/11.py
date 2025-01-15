e50e3d52f7f94ab9f6adc7f4331bccea6ff149d6
sowingautumn <2311408446@qq.com>
2025-01-15 17:28:11 +0800

    Add files via upload

---
+++ Task2_PythonBasic/11.py
@@ -0,0 +1,40 @@
+def Read_data():
+    list_data = []
+    with open('data.txt', 'r', encoding = 'utf-8')as f:
+        data = f.read()
+    for i in data.split():
+        list_data.append(float(i))
+    return list_data
+
+def Count(List):
+    cnt = 0
+    for i in List:
+        cnt += 1
+    return cnt
+
+def Avg(List, count):
+    sum = 0
+    for i in List:
+        sum += i
+    return sum / count
+
+def variance(List, count):
+    sum = 1
+    for i in List:
+        sum *= i
+    return sum / count
+
+def save(t1, t2 ,t3):
+    with open('ans.txt', 'w', encoding = 'utf-8')as f:
+        f.write(f'{t1}   {t2}    {t3}')
+
+
+List_data = Read_data()
+
+cnt = Count(List_data)
+
+avg = Avg(List_data, cnt)
+
+var = variance(List_data, cnt)
+
+save(cnt ,avg, var)
